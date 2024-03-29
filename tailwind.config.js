77ad665e372e9f1d9a0491a2404d7bf2c3d51db8
moathima <moathima86@gmail.com>
2024-03-30 00:59:23 +0300

    moath

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,20 @@ extend: {
        soft: "#676879",
        dark: "#808080",
        Neutral:"#FAFAFB",
+       node:"#E0E1E2",
+       basic:"#979797",
+       bono:"#F2F2F2"
      },
     black: {
-       soft: "#121212"
+       soft: "#121212",
+       dark:"#18202F"
     },
     pink: {
        soft: "#FF626A"
     },
     red: {
-       soft: "#ED4D55"
+       soft: "#ED4D55",
+       dark:"#F44A4A"
     },
 
     green:{
